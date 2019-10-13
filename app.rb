8d91311100577bf9409ca1b12ca954d84ebad3e3
Watayo <ryo0616mani@gmail.com>
2019-10-13 02:30:56 +0000

    何回やらすねん

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
-require 'pry'
+#require 'pry'
 require 'sinatra/activerecord'
 require './models'
 require 'date'
